dbb122bea4578b871f4108854b6c8995dff0e507
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-06-12 14:25:11 -0400

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hi my name is Heidi and I'm 25 years old");
+function whoAmI(name, age) {
+	const yearOfBirth = 2017 - age;
+	console.log("Hi my name is Heidi and I'm 25 years old");
+	console.log(`I was born in ${yearOfBirth}`);
+	if (age < 0) {
+		throw new Error("Age cannot be negative");
+	} else if (age === undefined || name === undefined) {
+		console.log("Arguments not valid");
+	} else {
+	return age;
+	}
+}
+
+
+whoAmI("Heidi");
